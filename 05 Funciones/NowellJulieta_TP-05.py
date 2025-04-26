fc9a21d9abc69b2c56d650e494b620ae1b58254f
JulietaNowell <julietanowell@gmail.com>
2025-04-26 12:05:47 -0300

    Update and rename MI-APELLIDO_TP-05.py to NowellJulieta_TP-05.py

---
+++ 05 Funciones/NowellJulieta_TP-05.py
@@ -0,0 +1,58 @@
+# EJERCICIO 1
+multiplos_de_4 = list(range(4,101,4))
+print(multiplos_de_4)
+
+# EJERCICIO 2
+lista = ["1", "2", "3", "4", "5"]
+print(lista[3])
+
+# EJERCICIO 3
+lista_vacia = []
+lista_vacia.append("perro")
+lista_vacia.append("gato")
+lista_vacia.append("hamster")
+print(lista_vacia)
+
+# EJERCICIO 4
+animales = ["perro", "gato", "conejo", "pez"]
+animales[1]="loro"
+animales[3]="oso"
+print(animales)
+
+# EJERCICIO 5
+# En la linea 1, se define la lista numeros compuesta por 5 elementos.
+# En la linea 2, se elimina el numero mas grande de la lista.
+# En la linea 3, se imprime por pantalla la lista luego de la eliminacion.
+
+# EJERCICIO 6
+numeros = list(range(10, 31, 5))
+print(numeros[0:2])
+
+# EJERCICIO 7
+autos = ["sedan", "polo", "suran", "gol"]
+print(autos)
+autos[1]="hilux"
+autos[2]="bronco"
+print(autos)
+
+# EJERCICIO 8
+lista_vacia = []
+print(lista_vacia)
+lista_vacia.append(2*5)
+lista_vacia.append(2*10)
+lista_vacia.append(2*15)
+print(lista_vacia)
+
+# EJERCICIO 9
+compras = [["pan", "leche"], ["arroz", "fideos", "salsa"], ["agua"]]
+print(compras)
+compras[2].append("jugo")
+print(compras)
+compras[1][1] = "tallarines"
+print(compras)
+compras[0].remove("pan")
+print(compras)
+
+# EJERCICIO 10
+lista_anidada = [15, "true", [25.5, 57.9, 30.6], "false"]
+print(lista_anidada)
